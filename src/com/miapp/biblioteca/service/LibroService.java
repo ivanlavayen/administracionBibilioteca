b12ba4dc9d85c24514e41dc6c77f27864599a6dd
IVAN A. LAVAYEN <ivanalejolavayen@gmail.com>
2024-03-30 11:52:34 -0300

    trabajo en CRUD de LibroSErvice

--- src/com/miapp/biblioteca/service/LibroService.java
+++ src/com/miapp/biblioteca/service/LibroService.java
@@ -5,7 +5,7 @@ import com.miapp.biblioteca.Libro;
 import java.util.ArrayList;
 
 public class LibroService {
-   //arreglo con la coleccion de libros
+    //arreglo con la coleccion de libros
     private ArrayList<Libro> biblioteca;
 
     public LibroService(ArrayList<Libro> biblioteca) {
@@ -13,16 +13,31 @@ public class LibroService {
     }
     //metodos para trabajar con los libros de la biblioteca  CRUD
 
-      //1) Crear libros
+    //1) Crear libros   CREAT
 
-    public void crearLibro(String titulo, String autor, String ISBN, String genero){
-        Libro nuevoLibro=new Libro( titulo,  autor,  ISBN,  genero);
+    public void crearLibro(String titulo, String autor, String ISBN, String genero) {
+        Libro nuevoLibro = new Libro(titulo, autor, ISBN, genero);
         biblioteca.add(nuevoLibro);
     }
 
-    public  ArrayList<Libro> getBiblioteca(){
+    //2)listar libros   READ
+    public ArrayList<Libro> getBiblioteca() {
         return biblioteca;
     }
+
+    //3)actualizar libros  UPDATE
+    public void actualizarLibro(String nuevoTitulo, String nuevoAutor, String ISBN, String nuevoGenero) {
+        //recorro la biblioteca buscando un isbn ==
+        for(Libro libro:biblioteca){
+            if(libro.getISBN().equals(ISBN)){
+                libro.setTitulo(nuevoTitulo);
+                libro.setAutor(nuevoAutor);
+                libro.setGenero(nuevoGenero);
+            }
+        }
+    }
+
+    //4)Borrar libro  DELETE
 
 }
 
